1298b0a827d4ad37a6a79622bb56378f3a61fa7a
gableri <100177439+gableri@users.noreply.github.com>
2022-03-13 19:52:27 +0800

    Attempt to implement Node class

--- Node.cs
+++ Node.cs
@@ -1,15 +1,21 @@
 namespace StacknQueue
 {
+    
+    //Node class for the program 
     public class Node<T>
     {
         // Properties also known as smart fields are the equivalent of a private field in Java with a getter and a setter
-        public Node Next { get; set; }
-        public T Data { get; set; }
+        public Node<T>? Next { get; set; } //The question mark means it is nullable. Not sure if this the correct way to do it
+        
+        public T? Data { get; set; }  //Question mark makes the variable nullable 
 
-        public Node(T data)
+        public Node (T data)
         {
             this.Data = data;
+            Next = null; //Sets the next node as null 
         }
-        public Node(){}
+        
     }
+
+   
 }
--- Stak.cs
+++ Stak.cs
@@ -2,25 +2,32 @@ namespace StacknQueue
 {
     public class Stak<T>
     {
-        private Node top;
+        private Node<T>? top;
         public Stak(){
             top = null;
         }
-        public void Push(T data)
+        public async void Push(T data)
         {
             // implement adding to the stack
+            //Allocate Memory for new node 
+            //Add the data 
+            
+            Node<T> MyPush = new Node<T>(data); 
+            
+
+
         }
 
         public T Pop()
         {
             // implement deleting from the stack
-            return null;
+            //return null;
         }
 
         public T Peek()
         {
             //implement peeking from the top of the stack
-            return null;
+            //return null;
         }
     }
 }
