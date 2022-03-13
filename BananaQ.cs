55bba6d31f31dcbadff8371cc5b48ea70f1907ce
gableri <100177439+gableri@users.noreply.github.com>
2022-03-13 21:39:47 +0800

    Removed errors added a print method in stak
    Cleaned up errors and added a print method in stak

--- BananaQ.cs
+++ BananaQ.cs
@@ -2,15 +2,15 @@ namespace StacknQueue
 {
     public class BananaQ<T>
     {
-        private Node front;
-        private Node rear;
+        private Node<T> front;
+        private Node<T> rear;
 
         public void Enqueue(T data)
         {
             //implement adding to queue
         }
 
-        public T Dequeue()
+        public void /*T*/ Dequeue()
         {
             // implement removing from a queue
         }
--- Stak.cs
+++ Stak.cs
@@ -6,28 +6,40 @@ namespace StacknQueue
         public Stak(){
             top = null;
         }
-        public async void Push(T data)
+        public void Push(T data)
         {
             // implement adding to the stack
             //Allocate Memory for new node 
             //Add the data 
             
             Node<T> MyPush = new Node<T>(data); 
+            top.Data = data; 
+            top = top.Next; 
             
 
 
         }
 
-        public T Pop()
+        public void /*T*/ Pop() //void return type for now. change to T when done implementing 
         {
             // implement deleting from the stack
             //return null;
         }
 
-        public T Peek()
+        public void /*T*/ Peek() //void return type for now. change to T when done implementing 
         {
             //implement peeking from the top of the stack
             //return null;
         }
+
+        
+        public void printMystack(){ //METHOD TO PRINT THE STACK (FOR TESTING PURPOSES) TO BE TESTED IF THIS WORKS 
+            Node<T>? current = top; 
+            while(current != null){
+                    Console.WriteLine(current.Data);
+                    current = current.Next;
+            }
+        }
     }
+    
 }
