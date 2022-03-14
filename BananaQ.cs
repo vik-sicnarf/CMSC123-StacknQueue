6309dfabec0b958de6833e435ebb8e40be587669
Victor Francis De Asis <vpdeasis@up.edu.ph>
2022-03-15 01:53:16 +0800

    Implement Enqueue and Dequeue
    Updated Program.cs for queue implementation

--- BananaQ.cs
+++ BananaQ.cs
@@ -2,17 +2,55 @@ namespace StacknQueue
 {
     public class BananaQ<T>
     {
-        private Node<T> front;
-        private Node<T> rear;
+        private Node<T>? front;
+        private Node<T>? rear;
+
+        public BananaQ() {
+            front = rear = null;
+        }
 
         public void Enqueue(T data)
         {
             //implement adding to queue
+            Node<T>? newElement = new Node<T>(data);
+
+            if(rear == null) { //For empty queue, front and rear are both the new node
+                front = rear = newElement;
+            } else { //Adds new node to the end of the queue, changing the rear node
+                rear.Next = newElement;
+                rear = newElement;
+            }
+            Console.WriteLine("Element to be added: " +data); //For testing the method
         }
 
-        public void /*T*/ Dequeue()
+        public T Dequeue()
         {
             // implement removing from a queue
+            
+            if(front == null){ //For empty queue, method will throw exception
+                throw new NullReferenceException("The queue is empty");
+            }
+
+            //Store the front to a temporary node and moves front one node ahead
+            T data = front.Data;
+            front = front.Next;
+
+            if(front == null){ //In case of all node being dequeued, set rear to null
+                rear = null;
+            }
+
+            Console.WriteLine(data + " has been deleted from the queue"); //For testing the method
+            return data;
+        }
+
+        public void printMyQueue()
+        {
+            Node<T>? current = front;
+            
+            while(current != null) {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,5 +14,17 @@ mytesty.printMystack();
 mytesty.Pop();
 mytesty.Peek(); 
 
+BananaQ<String> mytesty2 = new BananaQ<string>();
+mytesty2.Enqueue("SANA");
+mytesty2.Enqueue("GUMANA");
+mytesty2.Enqueue("YUNG");
+mytesty2.Enqueue("CODE");
+mytesty2.Dequeue();
+// mytesty2.Dequeue();
+// mytesty2.Dequeue();
+// mytesty2.Dequeue();
+// mytesty2.Dequeue(); //test Dequeue throw Exception
+mytesty2.printMyQueue();
+
 
 
